278868713b7f872708ea8e10fe39e2ad644bd666
Ariel DOSSOU <arieldossou00@gmail.com>
2022-01-20 12:10:10 +0100

    2nd Commit Loader

--- src/Components/Cp/Cp.js
+++ src/Components/Cp/Cp.js
@@ -7,25 +7,21 @@ import Experience from "../Experience/Experience";
 import Portfolio from "../Portfolio/Portfolio";
 import Contact from "../Contact/Contact";
 import Footer from "../Footer/Footer";
-import Loader from "../Loader/Loader";
-import Cp from "../Cp/Cp";
 
+function Cp() {
+    return (
+        <>
+                <Navbar/>
+                <Header/>
+                <About/>
+                <Service/>
+                <Experience/>
+                <Portfolio/>
+                <Contact/>
+                <Footer/>
+        </>
 
-function App() {
-    const[loading, setLoading] = useState(true);
-    console.log(loading);
-    setTimeout(() =>{
-        setLoading(false);
-    }, 2000);
-    console.log(loading);
-  return (
-      <>
-          {loading ? <Loader/> : <Cp/>
-
-          }
-      </>
-
-  );
+    );
 }
 
-export default App;
+export default Cp;
